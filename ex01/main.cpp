7177b7ebcdb0db0a00d2ece0cbcdc32c46c57007
nemoNephew <achraf.hamoutahar@gmail.com>
2026-09-01 19:07:03 +0100

    Refactor main function for RPN calculator

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,11 +1,13 @@
 #include "RPN.hpp"
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     if (argc != 2) {
-        std::cerr << "Error: Bad arguments." << std::endl;
+        std::cerr << "Error" << std::endl;
         return 1;
     }
-    RPN calculator;
-    calculator.calculate(argv[1]);
+
+    RPN rpn;
+    rpn.calculate(argv[1]);
+
     return 0;
 }
