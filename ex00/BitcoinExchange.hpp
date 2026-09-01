d1caf0e45b23bac7a72c271cfcd842b163e90b78
nemoNephew <achraf.hamoutahar@gmail.com>
2026-09-01 18:47:24 +0100

    Refactor includes and remove duplicate trim function

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -2,16 +2,19 @@
 #define BITCOINEXCHANGE_HPP
 
 #include <iostream>
-#include <map>
 #include <string>
+#include <map>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
 
 class BitcoinExchange {
 private:
     std::map<std::string, float> _db;
 
+    void trim(std::string& str) const;
     bool isValidDate(const std::string& date) const;
     bool isValidValue(const std::string& valueStr, float& value) const;
-    void trim(std::string& str) const;
 
 public:
     BitcoinExchange();
